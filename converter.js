a58d25327d33968de9a7a15c906aa99771df23a2
WilliBobadilla <willi1997.1@gmail.com>
2021-09-07 08:24:30 -0400

    first upload of project

---
+++ converter.js
@@ -0,0 +1,28 @@
+Object.defineProperty(Array.prototype, 'flat', {
+    value: function (depth = 1) {
+        return this.reduce(function (flat, toFlatten) {
+            return flat.concat((Array.isArray(toFlatten) && (depth > 1)) ? toFlatten.flat(depth - 1) : toFlatten);
+        }, []);
+    }
+});
+
+const p2s = require('postman-to-swagger')
+const yaml = require('js-yaml')
+const fs = require('fs')
+const postmanJson = require('./test.json')
+const swaggerJson = p2s(postmanJson, {
+    target_spec: "swagger2.0",
+    info: {
+        version: 'v1'
+    }
+})
+
+//let output = JSON.stringify(swaggerJson, null, 2)
+let output = yaml.dump(swaggerJson)
+
+// Save to file
+fs.writeFileSync(
+    'swaggerTest.yaml',
+    output,
+    'utf8'
+)
