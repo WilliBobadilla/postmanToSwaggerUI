51cd61582e492853e993c8152b1a8fa8dab38a8a
WilliBobadilla <willi1997.1@gmail.com>
2021-11-01 17:35:18 -0300

    organizing code

--- controllers/index.js
+++ controllers/index.js
@@ -2,31 +2,27 @@
 const p2s = require('.././src/index')//require('postman-to-swagger')//
 const yaml = require('js-yaml')
 const fs = require('fs')
+Object.defineProperty(Array.prototype, 'flat', {
+    value: function (depth = 1) {
+        return this.reduce(function (flat, toFlatten) {
+            return flat.concat((Array.isArray(toFlatten) && (depth > 1)) ? toFlatten.flat(depth - 1) : toFlatten);
+        }, []);
+    }
+});
+
 async function parse(req, res, next) {
     try {//logic here
         let inputForJsonConvert = JSON.parse(req.body.jsonField)
-        Object.defineProperty(Array.prototype, 'flat', {
-            value: function (depth = 1) {
-                return this.reduce(function (flat, toFlatten) {
-                    return flat.concat((Array.isArray(toFlatten) && (depth > 1)) ? toFlatten.flat(depth - 1) : toFlatten);
-                }, []);
-            }
-        });
         const swaggerJson = p2s(inputForJsonConvert, {
             target_spec: "swagger2.0",
             info: {
                 version: 'v1'
             }
         })
-
-        //let output = JSON.stringify(swaggerJson, null, 2)
         let output = yaml.dump(swaggerJson)
         res.render('parse', { resultOuput: output, input: JSON.stringify(inputForJsonConvert) });
     } catch (error) {
-        res.status(500).json({
-            error: error,
-            message: "There was an error",
-        });
+        res.render('parse', { resultOuput: "Error", input: "Error" });
     }
 }
 
